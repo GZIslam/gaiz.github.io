9d6ce1730657c037c00ac869245784f360954bbf
Gaibullaev Islam <Gaibullaeviz@gmail.com>
2019-06-16 19:13:44 +0300

    added resizing brush

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function drawall(ctx, e, coords, canv, color){
+function drawall(ctx, e, coords, canv, color, radius){
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, canv.width, canv.height );
     ctx.beginPath();
     coords.forEach(function(b){
-        ctx.lineWidth = 20;
+        ctx.lineWidth = b.radius * 2;
         ctx.fillStyle = b.color;
         ctx.strokeStyle = b.color;
         if (!b.break ) {
@@ -11,7 +11,7 @@ function drawall(ctx, e, coords, canv, color){
             ctx.stroke();
         }
         ctx.beginPath();
-        ctx.arc(b.x, b.y, 10, 0, Math.PI * 2);
+        ctx.arc(b.x, b.y, b.radius, 0, Math.PI * 2);
         ctx.fill();
         ctx.beginPath();
         ctx.moveTo(b.x, b.y);
@@ -19,7 +19,7 @@ function drawall(ctx, e, coords, canv, color){
     ctx.lineWidth = 1;
     ctx.strokeStyle = "red";
     ctx.beginPath();
-    ctx.arc(e.clientX, e.clientY, 10, 0, Math.PI * 2);
+    ctx.arc(e.clientX, e.clientY, radius, 0, Math.PI * 2);
     ctx.stroke();
 }
 
@@ -28,12 +28,16 @@ $(function(){
         canv = document.getElementById('canvas'),
         ctx = canv.getContext('2d'),
         color = 'black',
+        radius = 10,
         isMouseDown = false,
         coords = [];
     canv.width = window.innerWidth;
     canv.height = window.innerHeight;
     document.getElementById('color').oninput = function(){
         color = this.value;
+    }
+    document.getElementById('radius').oninput = function(){
+        radius = this.value;
     }
     document.getElementById('clear').onclick = function(){
         ctx.fillStyle = "white";
@@ -43,15 +47,15 @@ $(function(){
     }
     canv.addEventListener('mousedown', function(e){
         isMouseDown = true;
-        coords.push({x: e.clientX, y: e.clientY, break: true, color: color});
+        coords.push({x: e.clientX, y: e.clientY, break: true, color: color, radius: radius});
     });
     canv.addEventListener('mouseup', function(e){
         isMouseDown = false;
     });
     canv.addEventListener('mousemove', function(e){
-        drawall(ctx, e, coords, canv, color);
+        drawall(ctx, e, coords, canv, color, radius);
         if (isMouseDown){
-            coords.push({x: e.clientX, y: e.clientY,});
+            coords.push({x: e.clientX, y: e.clientY, color: color, radius: radius});
         }
     });
 });
