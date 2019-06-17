86672f7ddf830b2bd21d39f5c0b42384f67c893c
Gaibullaev Islam <Gaibullaeviz@gmail.com>
2019-06-17 17:13:14 +0300

    fixed changing the brush size by scrolling

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function drawall(ctx, e, coords, canv, count){
+function drawall(ctx, e, coords, canv, radius){
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, canv.width, canv.height );
     ctx.beginPath();
     coords.forEach(function(b){
-        ctx.lineWidth = b.count *2;
+        ctx.lineWidth = b.radius *2;
         ctx.fillStyle = 'black';
         ctx.strokeStyle = 'black';
         if (!b.break ) {
@@ -11,7 +11,7 @@ function drawall(ctx, e, coords, canv, count){
             ctx.stroke();
         }
         ctx.beginPath();
-        ctx.arc(b.x, b.y, b.count, 0, Math.PI * 2);
+        ctx.arc(b.x, b.y, b.radius, 0, Math.PI * 2);
         ctx.fill();
         ctx.beginPath();
         ctx.moveTo(b.x, b.y);
@@ -19,8 +19,27 @@ function drawall(ctx, e, coords, canv, count){
     ctx.lineWidth = 1;
     ctx.strokeStyle = "red";
     ctx.beginPath();
-    ctx.arc(e.clientX, e.clientY, count, 0, Math.PI * 2);
+    ctx.arc(e.clientX, e.clientY, radius, 0, Math.PI * 2);
     ctx.stroke();
+}
+
+function clear(ctx, e, coords, canv, radius)
+{
+    coords.forEach(function(b){
+        ctx.lineWidth = b.radius *2;
+        ctx.fillStyle = 'white';
+        ctx.strokeStyle = 'white';
+        if (!b.break ) {
+            ctx.lineTo(b.x, b.y);
+            ctx.stroke();
+        }
+        ctx.beginPath();
+        ctx.arc(b.x, b.y, b.radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.beginPath();
+        ctx.moveTo(b.x, b.y);
+    });
+    coords.length = 0;
 }
 
 $(function(){
@@ -28,44 +47,39 @@ $(function(){
         canv = document.getElementById('canvas'),
         ctx = canv.getContext('2d'),
         isMouseDown = false,
-        count = 10;
+        radius = 10;
         coords = [];
     canv.width = window.innerWidth ;
     canv.height = window.innerHeight;
-    function clear()
-    {
-        ctx.fillStyle = "white";
-        ctx.fillRect(0, 0, canv.width, canv.height );
-        ctx.beginPath();
-        coords.length = 0;
-    }
-    document.onwheel = function(event){
-        if (event.deltaY > 0){
-            count--;
-            if( count < 5){
-                count ++;
+    document.onwheel = function(e){
+        if (e.deltaY > 0){
+            radius--;
+            if( radius < 5){
+                radius ++;
             }
         }
         else{
-            count++;
+            radius++;
         }
+        drawall(ctx, e, coords, canv, radius);
     }
     canv.addEventListener('mousedown', function(e){
         isMouseDown = true;
-        coords.push({x: e.clientX, y: e.clientY, break: true, count: count});
+        coords.push({x: e.clientX, y: e.clientY, break: true, radius: radius});
     });
     canv.addEventListener('mouseup', function(e){
         isMouseDown = false;
+        drawall(ctx, e, coords, canv, radius);
     });
     canv.addEventListener('mousemove', function(e){
-        drawall(ctx, e, coords, canv, count);
+        drawall(ctx, e, coords, canv, radius);
         if (isMouseDown){
-            coords.push({x: e.clientX, y: e.clientY, count: count});
+            coords.push({x: e.clientX, y: e.clientY, radius: radius});
         }
     });
     document.addEventListener('keydown', function(e){
         if(e.keyCode === 46){
-            clear();
+            clear(ctx, e, coords, canv, radius);
             console.log('Cleared');
         }
     })
