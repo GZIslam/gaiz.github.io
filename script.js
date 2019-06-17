c95bacea4cff0fa47dcbeb5c90b69c9032534ed7
Gaibullaev Islam <Gaibullaeviz@gmail.com>
2019-06-17 15:44:02 +0300

    added change the brush size by scrolling

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function drawall(ctx, e, coords, canv,){
+function drawall(ctx, e, coords, canv, count){
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, canv.width, canv.height );
     ctx.beginPath();
     coords.forEach(function(b){
-        ctx.lineWidth = 20;
+        ctx.lineWidth = b.count *2;
         ctx.fillStyle = 'black';
         ctx.strokeStyle = 'black';
         if (!b.break ) {
@@ -11,7 +11,7 @@ function drawall(ctx, e, coords, canv,){
             ctx.stroke();
         }
         ctx.beginPath();
-        ctx.arc(b.x, b.y, 10, 0, Math.PI * 2);
+        ctx.arc(b.x, b.y, b.count, 0, Math.PI * 2);
         ctx.fill();
         ctx.beginPath();
         ctx.moveTo(b.x, b.y);
@@ -19,7 +19,7 @@ function drawall(ctx, e, coords, canv,){
     ctx.lineWidth = 1;
     ctx.strokeStyle = "red";
     ctx.beginPath();
-    ctx.arc(e.clientX, e.clientY, 10, 0, Math.PI * 2);
+    ctx.arc(e.clientX, e.clientY, count, 0, Math.PI * 2);
     ctx.stroke();
 }
 
@@ -28,6 +28,7 @@ $(function(){
         canv = document.getElementById('canvas'),
         ctx = canv.getContext('2d'),
         isMouseDown = false,
+        count = 10;
         coords = [];
     canv.width = window.innerWidth ;
     canv.height = window.innerHeight;
@@ -38,17 +39,28 @@ $(function(){
         ctx.beginPath();
         coords.length = 0;
     }
+    document.onwheel = function(event){
+        if (event.deltaY > 0){
+            count--;
+            if( count < 5){
+                count ++;
+            }
+        }
+        else{
+            count++;
+        }
+    }
     canv.addEventListener('mousedown', function(e){
         isMouseDown = true;
-        coords.push({x: e.clientX, y: e.clientY, break: true});
+        coords.push({x: e.clientX, y: e.clientY, break: true, count: count});
     });
     canv.addEventListener('mouseup', function(e){
         isMouseDown = false;
     });
     canv.addEventListener('mousemove', function(e){
-        drawall(ctx, e, coords, canv);
+        drawall(ctx, e, coords, canv, count);
         if (isMouseDown){
-            coords.push({x: e.clientX, y: e.clientY});
+            coords.push({x: e.clientX, y: e.clientY, count: count});
         }
     });
     document.addEventListener('keydown', function(e){
