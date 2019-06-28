e9eadebc171bafaa3d2e0a4437087fbfb20ecb18
Gaibullaev Islam <Gaibullaeviz@gmail.com>
2019-06-28 14:00:05 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-  function drawall(ctx, e, coords, canv, radius){
+  function drawall(ctx, e, coords, canv, radius, mycolor){
       ctx.fillStyle = "#090909";
       ctx.fillRect(0, 0, canv.width, canv.height );
       ctx.beginPath(),
@@ -30,9 +30,6 @@ $(function(){
         isMouseDown = false,
         radius = 10,
         coords = [];
-        document.getElementById('color').oninput = function () {
-          mycolor = this.value;
-        }
         coords [1] = {x: 795, y: 185, break: true, radius: 20},
 coords[2]= {x: 480, y: 249, break: true, radius: 20},
 coords[3]={x: 710, y: 304, break: true, radius: 20},
@@ -55,6 +52,9 @@ coords[8]={x: 110, y: 64, break: true, radius: 25},
         }
         drawall(ctx, e, coords, canv, radius, mycolor);
     }
+    document.getElementById('color').oninput = function () {
+      mycolor = this.value;
+    }
     canv.addEventListener('mousedown', function(e){
         isMouseDown = true;
         coords.push({x: e.clientX -337, y: e.clientY-407, break: true, radius: radius, mycolor: mycolor});
@@ -72,7 +72,7 @@ coords[8]={x: 110, y: 64, break: true, radius: 25},
     document.addEventListener('keydown', function(e){
         if (e.keyCode === 46){
             coords.length = 0;
-            drawall(ctx, e, coords, canv, radius);
+            drawall(ctx, e, coords, canv, radius, mycolor);
         }
     })
 });
